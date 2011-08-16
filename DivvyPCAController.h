7e814ea8fd74543d1f5ddb9f1c11d3b9afa83c1c
lvdmaaten <lvdmaaten@gmail.com>
2011-08-16 13:06:04 +0000

    Worked on PCA implementation.

---
+++ DivvyPCAController.h
@@ -0,0 +1,16 @@
+//
+//  DivvyPCAController.h
+//  Divvy
+//
+//  Created by Laurens van der Maaten on 8/16/11.
+//  Copyright 2011 Delft University of Technology. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface DivvyPCAController : NSViewController
+
+-(IBAction) changeK:(id)sender;
+
+@end
