54032441858b3d2105d294d91e7b568c058a98c8
kevestun <kevestun@8b32dacb-a295-4d3e-c631-f2bbff7c4245>
2011-10-20 21:58:21 +0000

    Initial processing on dataset views after being created is now on a separate thread and doesn't hold up the UI.

--- DivvyDatasetWindow.m
+++ DivvyDatasetWindow.m
@@ -48,7 +48,23 @@
   if (clickedSegmentTag == 0) { // Add button
     DivvyDatasetView *datasetView = [NSEntityDescription insertNewObjectForEntityForName:@"DatasetView" inManagedObjectContext:delegate.managedObjectContext];
     datasetView.dataset = delegate.selectedDataset;
-    [datasetView checkForNullPluginResults];
+    
+    [datasetView setProcessingImage];
+    [self.datasetViewsBrowser reloadData];  
+    
+    NSInvocationOperation *invocationOperation = [[[NSInvocationOperation alloc] initWithTarget:datasetView
+                                                                                       selector:@selector(checkForNullPluginResults)
+                                                                                         object:nil] autorelease];
+    
+    // Reload the DatasetView image in main thread once processing is complete.
+    [invocationOperation setCompletionBlock:^{
+      dispatch_async(dispatch_get_main_queue(), ^{
+        [self.datasetViewsBrowser reloadData];
+      });
+    }];
+    
+    
+    [datasetView.operationQueue addOperation:invocationOperation];    
   }
   else { // Remove button
     for (id datasetView in [self.datasetViewsArrayController selectedObjects])
